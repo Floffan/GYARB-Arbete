c3f6e86aeb2dd0b8829e9375fbf39f38229001de
Filippa <filippa.rolfsman@gmail.com>
2023-04-10 22:11:15 +0200

    några estetiska ändringar - la till snö + fxade dörrar + la till mer träd i main

--- Gyarb-projekt Version 1/Scripts/HUD/Transition.gd
+++ Gyarb-projekt Version 1/Scripts/HUD/Transition.gd
@@ -1,24 +1,11 @@
 extends CanvasLayer
 
-
 onready var animation_player = $AnimationPlayer
 
-onready var svart = $Control/Svart
-
-
-#func load_scene(path):
-	#animation_player.play("Fade")
-	#yield(animation_player, "animation_finished")
-	#get_tree().change_scene(path)
-	#animation_player.play_backwards("Fade")
-	#print("klar")
-	
-
 func load_scene(path, delay = 0.5):
-	#yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("Fade")
-	#yield(animation_player, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
 	animation_player.play_backwards("Fade")
 	
-	
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	animation_player.play("RESET")
